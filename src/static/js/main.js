cc0129317a0b0267de9adf0a7e8a94f6fc189829
AlxGreen <1.ooo.ooo@bk.ru>
2019-04-30 20:02:38 +0300

    5454

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
+	function checkVP () {
+		let vp = $('meta[name="viewport"]');
+		if (screen.width <= 600) {
+		vp.attr('content', 'width=425, user-scalable=no');
+		} else {
+		vp.attr('content', 'width=device-width, user-scalable=no');
+		};
+		}
+		$(window).on('resize', checkVP);
+		$(window).trigger('resize')
+	$('#sliderCourse').slick({
+		dots: false,
+		arrows: true,
+		fade:true,
+		nextArrow: '<div class="pages__next">следующий</div>',
+		prevArrow: '<div class="pages__pre">предыдущий</div>',
+	});
 	$('#slider01').slick({
 		dots: false,
 		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 200000
+		fade:true
 	});
 	$('#outcomesSlider').slick({
 		dots: false,
@@ -17,4 +33,21 @@ $(document).ready(function () {
 		autoplay: true,
 		autoplaySpeed: 200000
 	});
+	$('#modulsWr').slick({
+		dots: false,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 200000,
+	});
+	$('#companies').slick({
+		dots: false,
+		arrows: false,
+		arrows: true,
+		fade:true,
+		nextArrow: '<div class="pages__next">следующий</div>',
+		prevArrow: '<div class="pages__pre">предыдущий</div>',
+	});
+	if (screen.width >=1023) {
+		$('#modulsWr').slick('unslick');
+	}
 });

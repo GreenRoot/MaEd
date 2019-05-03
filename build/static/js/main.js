2932fda35a15927b6d4c052765b457f617cbcd28
AlxGreen <1.ooo.ooo@bk.ru>
2019-05-03 10:06:04 +0300

    save

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -50,4 +50,129 @@ $(document).ready(function () {
 	if (screen.width >=1023) {
 		$('#modulsWr').slick('unslick');
 	}
+
+
+	//Для кого курс
+
+	$('.chubaka-mini').click(function(){
+		$('.maxi-block-chubaka').show();
+		$('.maxi-block-c3po').hide();
+		$('.maxi-block-r2d2').hide();
+		$('.maxi-block-vader').hide();
+	});
+	$('.c3po-mini').click(function(){
+		$('.maxi-block-chubaka').hide();
+		$('.maxi-block-c3po').show();
+		$('.maxi-block-r2d2').hide();
+		$('.maxi-block-vader').hide();
+	});
+	$('.r2d2-mini').click(function(){
+		$('.maxi-block-chubaka').hide();
+		$('.maxi-block-c3po').hide();
+		$('.maxi-block-r2d2').show();
+		$('.maxi-block-vader').hide();
+	});
+	$('.vader-mini').click(function(){
+		$('.maxi-block-chubaka').hide();
+		$('.maxi-block-c3po').hide();
+		$('.maxi-block-r2d2').hide();
+		$('.maxi-block-vader').show();
+	});
+
+// Слайдер "8 причин"
+
+	$('.tog-img1').click(function(){
+		$('.tog-big-img1').show();
+		$('.tog-big-img2').hide();
+		$('.tog-big-img3').hide();
+		$('.tog-big-img4').hide();
+		$('.tog-big-img5').hide();
+		$('.tog-big-img6').hide();
+		$('.tog-big-img7').hide();
+		$('.tog-big-img8').hide();
+	});
+
+	$('.tog-img2').click(function(){
+		$('.tog-big-img1').hide();
+		$('.tog-big-img2').show();
+		$('.tog-big-img3').hide();
+		$('.tog-big-img4').hide();
+		$('.tog-big-img5').hide();
+		$('.tog-big-img6').hide();
+		$('.tog-big-img7').hide();
+		$('.tog-big-img8').hide();
+	});
+
+	$('.tog-img3').click(function(){
+		$('.tog-big-img1').hide();
+		$('.tog-big-img2').hide();
+		$('.tog-big-img3').show();
+		$('.tog-big-img4').hide();
+		$('.tog-big-img5').hide();
+		$('.tog-big-img6').hide();
+		$('.tog-big-img7').hide();
+		$('.tog-big-img8').hide();
+	});
+
+	$('.tog-img4').click(function(){
+		$('.tog-big-img1').hide();
+		$('.tog-big-img2').hide();
+		$('.tog-big-img3').hide();
+		$('.tog-big-img4').show();
+		$('.tog-big-img5').hide();
+		$('.tog-big-img6').hide();
+		$('.tog-big-img7').hide();
+		$('.tog-big-img8').hide();
+	});
+
+	$('.tog-img5').click(function(){
+		$('.tog-big-img1').hide();
+		$('.tog-big-img2').hide();
+		$('.tog-big-img3').hide();
+		$('.tog-big-img4').hide();
+		$('.tog-big-img5').show();
+		$('.tog-big-img6').hide();
+		$('.tog-big-img7').hide();
+		$('.tog-big-img8').hide();
+	});
+
+	$('.tog-img6').click(function(){
+		$('.tog-big-img1').hide();
+		$('.tog-big-img2').hide();
+		$('.tog-big-img3').hide();
+		$('.tog-big-img4').hide();
+		$('.tog-big-img5').hide();
+		$('.tog-big-img6').show();
+		$('.tog-big-img7').hide();
+		$('.tog-big-img8').hide();
+	});
+
+	$('.tog-img7').click(function(){
+		$('.tog-big-img1').hide();
+		$('.tog-big-img2').hide();
+		$('.tog-big-img3').hide();
+		$('.tog-big-img4').hide();
+		$('.tog-big-img5').hide();
+		$('.tog-big-img6').hide();
+		$('.tog-big-img7').show();
+		$('.tog-big-img8').hide();
+	});
+
+	$('.tog-img8').click(function(){
+		$('.tog-big-img1').hide();
+		$('.tog-big-img2').hide();
+		$('.tog-big-img3').hide();
+		$('.tog-big-img4').hide();
+		$('.tog-big-img5').hide();
+		$('.tog-big-img6').hide();
+		$('.tog-big-img7').hide();
+		$('.tog-big-img8').show();
+	});
+// паралаксы
+	var droid = document.getElementById('droid');
+	var tabletBlue = document.getElementById('tabletBlue');
+	var tabletRed = document.getElementById('tabletRed');
+	var droidParallaxInstance = new Parallax(droid, {relativeInput: true});
+	var tabletBlueParallaxInstance = new Parallax(tabletBlue, {relativeInput: true});
+	var tabletRedParallaxInstance = new Parallax(tabletRed, {relativeInput: true});
 });
